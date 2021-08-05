c6eca9dec000bd840e6f8a259f6dc9ee3a82768a
SourceHorse <rossscarborough@gmail.com>
2021-08-04 23:52:00 -0400

    new default and drag cursors

--- mappitweb/src/components/MappitMap.vue
+++ mappitweb/src/components/MappitMap.vue
@@ -10,18 +10,34 @@ export default {
   name: 'MappitMap',
   data() {
     return {
-      drawLayer: null,
-      mapView: null,
+      map: null,
     }
   },
   mounted() {
-    var map = L.map('mapDiv').setView([35.85, -78.8], 11);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    this.map = this.createMap();
+    this.map.on('click', this.mapClick);
+    this.map.on('dragstart', this.dragStart);
+    this.map.on('dragend', this.dragEnd);
   },
   methods: {
-
+    createMap() {
+      var map = L.map('mapDiv').setView([35.85, -78.8], 11);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      return map;
+    },
+    mapClick(e) {
+      console.log(e);
+    },
+    dragStart() {
+      document.getElementById('mapDiv').classList.add('drag');
+      console.log('dragging');
+    },
+    dragEnd() {
+      document.getElementById('mapDiv').classList.remove('drag');
+      console.log('not dragging');
+    }
   }
 }
 </script>
@@ -33,5 +49,10 @@ export default {
     left: 0;
     height: 100vh;
     width: 100vw;
+    cursor: pointer;
+
+    &.drag {
+      cursor: grabbing !important;
+    }
   }
 </style>
