4fb5ad7fa3c6d5df2f51a5fb10f351893891f79b
SourceHorse <rossscarborough@gmail.com>
2021-12-28 22:45:03 -0500

    add vuetify 3 alpha

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === "production" ? "/Mappit/" : "/",
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 };
